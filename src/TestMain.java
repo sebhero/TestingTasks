f60a95a302d9d23a073cb5701544a369d1d07aaa
seb <trevligt.att.ses@gmail.com>
2016-01-17 16:15:33 +0100

    Initial commit

---
+++ src/TestMain.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Sebastian Börebäck on 2016-01-17.
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		// TODO: 2016-01-17 asdf
+		// #TODO:10 hashtag task in a java comment
+		//#doing:20 task in a text
+
+		System.out.println("hello world");
+	}
+}
